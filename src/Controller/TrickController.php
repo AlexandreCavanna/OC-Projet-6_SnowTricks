f043ca1ecae0c3d5c2a91d83b765cc27ae24dc55
AlexandreCavanna <alexandre.cavanna.pro@gmail.com>
2021-06-13 17:46:10 +0200

    (feat): add homepage

---
+++ src/Controller/TrickController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TrickRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TrickController extends AbstractController
+{
+    /**
+     * @Route("/", name="trick_index", methods={"GET"})
+     */
+    public function index(TrickRepository $trickRepository): Response
+    {
+        return $this->render('trick/index.html.twig', [
+            'tricks' => $trickRepository->findAll(),
+        ]);
+    }
+}
